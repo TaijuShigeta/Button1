d15b4561332e4ae1544df39130cf70c033e95900
TaijuShigeta <kigyo1998company@gmail.com>
2019-08-08 18:50:50 +0900

    intern

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.helloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById(R.id.button) as Button
+        val textview = findViewById(R.id.aisatsutextView) as TextView
+        textview.text = "Hello World!!"
+
+        button.setOnClickListener { onClickButton() }
+    }
+
+    val onClickButton = {
+        val textView = findViewById(R.id.aisatsutextView) as TextView
+        textView.text = "Change!!"
     }
 }
